2039860a24ba40269615f42f1a68ba2966d54d7f
Ojas Sharma <sojas4459@gmail.com>
2026-05-03 12:04:09 +0530

    Start working on navigation

--- Akshara/src/com/texteditor/akshara/Akshara.java
+++ Akshara/src/com/texteditor/akshara/Akshara.java
@@ -23,7 +23,7 @@ public class Akshara {
 
 		while (true) {
 
-			refreshScreen();
+			//refreshScreen();
 			int key = readKey();
 			handleKey(key);
 
@@ -40,7 +40,9 @@ public class Akshara {
 		
 		StringBuilder builder =  new StringBuilder();
 		
-		builder.append("\033[2J");
+		//We use ANSI escape codes to manipulate the screen 
+		
+		builder.append("\033[2J"); 
 		builder.append("\033[H");
 
 		for (int i = 0; i < rows - 1; i++) {
@@ -64,21 +66,41 @@ public class Akshara {
 		if (key == 'q') {
 			System.out.print("\033[2J");
 			System.out.print("\033[H");
-			LibC.INSTANCE.tcsetattr(LibC.SYSTEM_OUT_FD, LibC.TCSAFLUSH, defaultAttributes);
+			LibC.INSTANCE.tcsetattr(LibC.SYSTEM_IN_FD, LibC.TCSAFLUSH, defaultAttributes);
 			System.exit(1);
+		} else {
+			System.out.print(((char)key) + "\r\n");
 		}
 
 	}
 
 	private static int readKey() throws IOException {
-		return System.in.read();
+		int key = System.in.read();
+		//check if the input is a escape sequence 
+//		if(key != '\033') {
+//			return key;
+//		}
+//		
+//		int nextKey = System.in.read();
+//		if(nextKey != '[') {
+//			return nextKey;
+//		}
+//		
+//		int anotherKey = System.in.read();
+//		switch(anotherKey) {
+////		case 'A' -> return ARROW_UP;
+////		case 'B' -> return ARROW_DOWN;
+//		default -> return key;
+//		}
+		return key;
 	}
 
 	private static void enableRawMode() {
 		// Getting the current attributes of the system
 		LibC.Termios termios = new LibC.Termios();
-		int returnCode = LibC.INSTANCE.tcgetattr(LibC.SYSTEM_OUT_FD, termios);
-
+		int returnCode = LibC.INSTANCE.tcgetattr(LibC.SYSTEM_IN_FD, termios);
+		
+		//Saving the copy of the current attributes of the terminal to restore cooked mode  
 		defaultAttributes = LibC.Termios.of(termios);
 
 		if (returnCode != 0) {
@@ -87,22 +109,25 @@ public class Akshara {
 			System.exit(1);
 		}
 
-		// With the help of bitwise negate the values such has turn off echoing,
-		// canonical etc...
+		// With the help of bitwise operation we negate the values, such has turn off echoing,
+		// canonical,implementation-defined input processing and signals
 		termios.c_lflag &= ~(LibC.ECHO | LibC.ICANON | LibC.IEXTEN | LibC.ISIG);
+		//Disable flow and stop the translations
 		termios.c_iflag &= ~(LibC.IXON | LibC.ICRNL);
+		//disable output post-processing
 		termios.c_oflag &= ~(LibC.OPOST);
 
+//These to control the read(), not useful for this application
 //		termios.c_cc[LibC.VMIN] = 0;
 //		termios.c_cc[LibC.VTIME] = 1;
 
-		returnCode = LibC.INSTANCE.tcsetattr(LibC.SYSTEM_OUT_FD, LibC.TCSAFLUSH, termios);
+		returnCode = LibC.INSTANCE.tcsetattr(LibC.SYSTEM_IN_FD, LibC.TCSAFLUSH, termios);
 
 	}
 
 	private static LibC.Winsize getWindowSize() {
 		final LibC.Winsize winsize = new LibC.Winsize();
-		final int returnCode = LibC.INSTANCE.ioctl(LibC.SYSTEM_OUT_FD, LibC.TIOCGWINSZ, winsize);
+		final int returnCode = LibC.INSTANCE.ioctl(LibC.SYSTEM_IN_FD, LibC.TIOCGWINSZ, winsize);
 
 		if (returnCode != 0) {
 			System.err.println("ioctl failed");
@@ -116,19 +141,49 @@ public class Akshara {
 
 
 
-//This interface will help use make direct Linux syscalls i.e., provide an interface between a process and the operating system
+/*
+ * This interface will help use make direct Linux syscalls i.e., provide an
+ * interface between a process and the operating system
+ */
 interface LibC extends Library {
-
-	int SYSTEM_OUT_FD = 0;
+	
+	//This is a constant that represents the default data stream input (stdin), here the kernel sees 0
+	//Terminal configurations need a file descriptor that points to the terminal device
+	int SYSTEM_IN_FD = 0;
+	
+	//These are octal bitmask values from Linux kernel headers, 
+	//meaning these are just octal values that represent these in the terminal setting 
+	
+	/* from Linux kernel asm/termbits.h */
+	/*#define ISIG    0000001  -> octal 
+	* #define ICANON  0000002
+	* #define ECHO    0000010
+	* #define IEXTEN  0100000
+	* #define IXON    0002000
+	* #define ICRNL   0000400
+	* #define OPOST   0000001 */
+	
+	
 	int ISIG = 1, ICANON = 2, ECHO = 10, TCSAFLUSH = 2, IXON = 2000, ICRNL = 400, IEXTEN = 100000, OPOST = 1, VMIN = 6,
-			VTIME = 5, TIOCGWINSZ = 0x5413;
+			VTIME = 5;
+	
+	//Stands for Terminal IO Control Get WINdow SiZe is a ioctl request code we need 
+	//to pass on the ioctl to get the window size
+	int  TIOCGWINSZ = 0x5413;
 	
 	
-	//This creates a dynamic proxy object and every method call on INSTANCE becomes a real native syscall under the hood
+	/*
+	 * This creates a dynamic proxy object and every method call on INSTANCE becomes
+	 * a real native syscall under the hood
+	 */
 	LibC INSTANCE = Native.load("c", LibC.class);
-
+	
+	//This mirrors the termios struct in memory so we can pass it to the sys calls
+	//@Structure.FieldOrder tell jna the memory layout since in C every thing is one block of memory
+	
 	@Structure.FieldOrder(value = { "c_iflag", "c_oflag", "c_cflag", "c_lflag", "c_cc" })
 	class Termios extends Structure {
+		//copied from the man page as it is 
 		public int c_iflag; /* input modes */
 		public int c_oflag; /* output modes */
 		public int c_cflag; /* control modes */
@@ -151,16 +206,23 @@ interface LibC extends Library {
 					+ c_lflag + ", c_cc=" + Arrays.toString(c_cc) + "]";
 		}
 	}
-
+	
+	//Mirrors the winsize struct in <sys/ioctl.h> to get the window size 
+	
 	@Structure.FieldOrder(value = { "ws_row", "ws_col", "ws_xpixel", "ws_ypixel" })
 	public class Winsize extends Structure {
 		public short ws_row, ws_col, ws_xpixel, ws_ypixel;
 	}
-
+	
+	//Native methods 
+	
+	//get the default terminal attributes
 	int tcgetattr(int fd, Termios termios);
-
+	
+	//set the terminal attributes
 	int tcsetattr(int fd, int optional_actions, Termios termios);
-
+	
+	//input and output control is the system call under Device management, device specific operations
 	int ioctl(int fd, int opt, Winsize winze);
 
 }
