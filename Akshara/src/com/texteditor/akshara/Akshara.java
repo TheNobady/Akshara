e6376bc303b6ce9ec05eb74473b675a3b2cef383
Ojas Sharma <sojas4459@gmail.com>
2026-05-04 00:19:29 +0530

    Map keys

--- Akshara/src/com/texteditor/akshara/Akshara.java
+++ Akshara/src/com/texteditor/akshara/Akshara.java
@@ -10,6 +10,16 @@ import com.sun.jna.Structure;
 public class Akshara {
 
 	public static final String APP_NAME = "Akshara";
+
+	private static final int HOME = 1000;
+	private static final int ARROW_UP = 1001;
+	private static final int ARROW_DOWN = 1002;
+	private static final int ARROW_RIGHT = 1003;
+	private static final int ARROW_LEFT = 1004;
+	private static final int PAGE_UP = 1005;
+	private static final int PAGE_DOWN =1006;
+	private static final int END = 1007;
+	
 	public static LibC.Termios defaultAttributes;
 	public static int rows = 10;
 	public static int columns = 10;
@@ -23,7 +33,7 @@ public class Akshara {
 
 		while (true) {
 
-			//refreshScreen();
+			// refreshScreen();
 			int key = readKey();
 			handleKey(key);
 
@@ -37,28 +47,24 @@ public class Akshara {
 	}
 
 	private static void refreshScreen() {
-		
-		StringBuilder builder =  new StringBuilder();
-		
-		//We use ANSI escape codes to manipulate the screen 
-		
-		builder.append("\033[2J"); 
+
+		StringBuilder builder = new StringBuilder();
+
+		// We use ANSI escape codes to manipulate the screen
+
+		builder.append("\033[2J");
 		builder.append("\033[H");
 
 		for (int i = 0; i < rows - 1; i++) {
 			builder.append(">\r\n");
 		}
-		
+
 		String statusbar = APP_NAME + " - " + VERSION;
-		builder.append("\033[7m")
-				.append(APP_NAME)
-				.append(" - ")
-				.append(VERSION)
-				.append(" ".repeat(Math.max(0, columns - statusbar.length())))
-				.append("\033[0m");
-		
+		builder.append("\033[7m").append(APP_NAME).append(" - ").append(VERSION)
+				.append(" ".repeat(Math.max(0, columns - statusbar.length()))).append("\033[0m");
+
 		builder.append("\033[H");
-		
+
 		System.out.println(builder);
 	}
 
@@ -69,38 +75,75 @@ public class Akshara {
 			LibC.INSTANCE.tcsetattr(LibC.SYSTEM_IN_FD, LibC.TCSAFLUSH, defaultAttributes);
 			System.exit(1);
 		} else {
-			System.out.print(((char)key) + "\r\n");
+			System.out.print(((char) key) + " -> " + key + "\r\n");
 		}
 
 	}
 
 	private static int readKey() throws IOException {
 		int key = System.in.read();
-		//check if the input is a escape sequence 
-//		if(key != '\033') {
-//			return key;
-//		}
-//		
-//		int nextKey = System.in.read();
-//		if(nextKey != '[') {
-//			return nextKey;
-//		}
-//		
-//		int anotherKey = System.in.read();
-//		switch(anotherKey) {
-////		case 'A' -> return ARROW_UP;
-////		case 'B' -> return ARROW_DOWN;
-//		default -> return key;
-//		}
-		return key;
+		// check if the input is a escape sequence
+		if (key != '\033') {
+			return key;
+		}
+
+		int nextKey = System.in.read();
+		if (nextKey != '[' && nextKey != 'O') {
+			return nextKey;
+		}
+
+		// catching arrow keys, home , end and page up down etc...
+		if (nextKey == '[') {
+			int anotherKey = System.in.read();
+			return switch ((char) anotherKey) {
+			case 'A' -> ARROW_UP;
+			case 'B' -> ARROW_DOWN;
+			case 'C' -> ARROW_RIGHT;
+			case 'D' -> ARROW_LEFT;
+			case 'H' -> HOME;
+			case 'E' -> END;
+			case '1','2','3','4','5','6','7','8','9' -> {
+				int andAnotherKey = System.in.read();
+				if(andAnotherKey != '~') {
+					yield andAnotherKey;
+				}
+				
+				switch(andAnotherKey) {
+				case '1':
+				case '7':
+					yield HOME;
+				case '3':
+					yield DEL;
+				case '4':
+				case '8':
+					yield END;
+				case '5':
+					yield PAGE_UP;
+				case '6':
+					yield PAGE_DOWN;
+				default: yield andAnotherKey;
+				}
+			}
+			default -> anotherKey;
+			};
+		}
+		
+		else {
+			return switch(nextKey) {
+			case 'H' -> HOME;
+			case 'E' -> END;
+			default -> nextKey;
+			};
+		}
 	}
 
 	private static void enableRawMode() {
 		// Getting the current attributes of the system
 		LibC.Termios termios = new LibC.Termios();
 		int returnCode = LibC.INSTANCE.tcgetattr(LibC.SYSTEM_IN_FD, termios);
-		
-		//Saving the copy of the current attributes of the terminal to restore cooked mode  
+
+		// Saving the copy of the current attributes of the terminal to restore cooked
+		// mode
 		defaultAttributes = LibC.Termios.of(termios);
 
 		if (returnCode != 0) {
@@ -109,12 +152,13 @@ public class Akshara {
 			System.exit(1);
 		}
 
-		// With the help of bitwise operation we negate the values, such has turn off echoing,
+		// With the help of bitwise operation we negate the values, such has turn off
+		// echoing,
 		// canonical,implementation-defined input processing and signals
 		termios.c_lflag &= ~(LibC.ECHO | LibC.ICANON | LibC.IEXTEN | LibC.ISIG);
-		//Disable flow and stop the translations
+		// Disable flow and stop the translations
 		termios.c_iflag &= ~(LibC.IXON | LibC.ICRNL);
-		//disable output post-processing
+		// disable output post-processing
 		termios.c_oflag &= ~(LibC.OPOST);
 
 //These to control the read(), not useful for this application
@@ -139,51 +183,50 @@ public class Akshara {
 	}
 }
 
-
-
 /*
  * This interface will help use make direct Linux syscalls i.e., provide an
  * interface between a process and the operating system
  */
 interface LibC extends Library {
-	
-	//This is a constant that represents the default data stream input (stdin), here the kernel sees 0
-	//Terminal configurations need a file descriptor that points to the terminal device
+
+	// This is a constant that represents the default data stream input (stdin),
+	// here the kernel sees 0
+	// Terminal configurations need a file descriptor that points to the terminal
+	// device
 	int SYSTEM_IN_FD = 0;
-	
-	//These are octal bitmask values from Linux kernel headers, 
-	//meaning these are just octal values that represent these in the terminal setting 
-	
+
+	// These are octal bitmask values from Linux kernel headers,
+	// meaning these are just octal values that represent these in the terminal
+	// setting
+
 	/* from Linux kernel asm/termbits.h */
-	/*#define ISIG    0000001  -> octal 
-	* #define ICANON  0000002
-	* #define ECHO    0000010
-	* #define IEXTEN  0100000
-	* #define IXON    0002000
-	* #define ICRNL   0000400
-	* #define OPOST   0000001 */
-	
-	
+	/*
+	 * #define ISIG 0000001 -> octal #define ICANON 0000002 #define ECHO 0000010
+	 * #define IEXTEN 0100000 #define IXON 0002000 #define ICRNL 0000400 #define
+	 * OPOST 0000001
+	 */
+
 	int ISIG = 1, ICANON = 2, ECHO = 10, TCSAFLUSH = 2, IXON = 2000, ICRNL = 400, IEXTEN = 100000, OPOST = 1, VMIN = 6,
 			VTIME = 5;
-	
-	//Stands for Terminal IO Control Get WINdow SiZe is a ioctl request code we need 
-	//to pass on the ioctl to get the window size
-	int  TIOCGWINSZ = 0x5413;
-	
-	
+
+	// Stands for Terminal IO Control Get WINdow SiZe is a ioctl request code we
+	// need
+	// to pass on the ioctl to get the window size
+	int TIOCGWINSZ = 0x5413;
+
 	/*
 	 * This creates a dynamic proxy object and every method call on INSTANCE becomes
 	 * a real native syscall under the hood
 	 */
 	LibC INSTANCE = Native.load("c", LibC.class);
-	
-	//This mirrors the termios struct in memory so we can pass it to the sys calls
-	//@Structure.FieldOrder tell jna the memory layout since in C every thing is one block of memory
-	
+
+	// This mirrors the termios struct in memory so we can pass it to the sys calls
+	// @Structure.FieldOrder tell jna the memory layout since in C every thing is
+	// one block of memory
+
 	@Structure.FieldOrder(value = { "c_iflag", "c_oflag", "c_cflag", "c_lflag", "c_cc" })
 	class Termios extends Structure {
-		//copied from the man page as it is 
+		// copied from the man page as it is
 		public int c_iflag; /* input modes */
 		public int c_oflag; /* output modes */
 		public int c_cflag; /* control modes */
@@ -206,23 +249,24 @@ interface LibC extends Library {
 					+ c_lflag + ", c_cc=" + Arrays.toString(c_cc) + "]";
 		}
 	}
-	
-	//Mirrors the winsize struct in <sys/ioctl.h> to get the window size 
-	
+
+	// Mirrors the winsize struct in <sys/ioctl.h> to get the window size
+
 	@Structure.FieldOrder(value = { "ws_row", "ws_col", "ws_xpixel", "ws_ypixel" })
 	public class Winsize extends Structure {
 		public short ws_row, ws_col, ws_xpixel, ws_ypixel;
 	}
-	
-	//Native methods 
-	
-	//get the default terminal attributes
+
+	// Native methods
+
+	// get the default terminal attributes
 	int tcgetattr(int fd, Termios termios);
-	
-	//set the terminal attributes
+
+	// set the terminal attributes
 	int tcsetattr(int fd, int optional_actions, Termios termios);
-	
-	//input and output control is the system call under Device management, device specific operations
+
+	// input and output control is the system call under Device management, device
+	// specific operations
 	int ioctl(int fd, int opt, Winsize winze);
 
 }
